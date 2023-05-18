c11ddcc7d46941bb17157892023c2581bc0b6581
Andres Martinez <andres.martinez@habil.mx>
2023-05-17 19:27:53 -0600

    primer intento de libreeria de hooks

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import packageJson from './package.json' assert { type: 'json' }
+//import external from 'rollup-plugin-peer-deps-external'
+
+//const packageJson = require('./package.json')
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'named',
+        //strict: false
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [resolve(), commonjs(), typescript({ tsconfig: './tsconfig.json' }), commonjs()],
+    external: ['react', 'react-dom'],
+  },
+  {
+    input: 'dist/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/],
+  },
+]
