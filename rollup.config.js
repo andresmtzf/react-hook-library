f55105ea6a852aa5078107dec1004d4aa458b21d
andres.martinez <andres_mf1@live.com.mx>
2023-05-18 19:17:35 -0600

    cambia rollup

--- rollup.config.js
+++ rollup.config.js
@@ -11,14 +11,19 @@ import sass from 'rollup-plugin-sass'
 export default [
   {
     input: './src/index.ts',
-    output: {
-      file: 'dist/index.js',
-      format: 'umd',
-      name: 'and-lib-hooks-v4',
-      globals: { react: 'React' },
-      sourcemap: 'inline',
-      exports: 'named',
-    },
+    output: [
+      {
+        file: 'dist/index.js',
+        format: 'cjs',
+        sourcemap: 'true',
+        exports: 'named',
+      },
+      {
+        file: 'dist/index.esm.js',
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
     plugins: [
       resolve(),
       commonjs(),
